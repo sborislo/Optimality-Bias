80cc121a49401d15a023f3269d2f12bca73b0781
sborislo <sbori@chicagobooth.edu>
2023-01-19 15:54:18 -0600

    1-19_in-class_Cleaning
    Cleaned dataset more. R script also updated.
    
    Co-Authored-By: Jenny Bo <89056736+hjennybo@users.noreply.github.com>
    Co-Authored-By: Natalie Dowling <35471398+nrdowling@users.noreply.github.com>

--- 1-19_in-class/1-19_in-class_R_Script.R
+++ 1-19_in-class/1-19_in-class_R_Script.R
@@ -9,7 +9,18 @@ for (i in 10:11) {
   optdata[,i] <- sapply(optdata[,i], as.character)
 }
 
+#change conditions to labels
+optdata$choice = as_factor(choice)
+optdata$choice = factor(optdata$choice, labels = c("Optimal", "Suboptimal"))
+optdata$expert = as_factor(expert)
+optdata$expert = factor(optdata$expert, labels = c("Expert", "Non-expert"))
+
 write.csv(optdata, "C:/Users/samha/Documents/GitHub/Optimality-Bias/1-19_in-class_CLEANED DATASHEET.csv")
 
 #NOTE: I AM PURPOSELY NOT WRITING OUT THESE PARTS BECAUSE IT'S JUST FOR PRACTICE
-which(optdata$isl_pkc != 9.00 & optdata$isl_pkc != 1.00)
+ggplot(optdata, aes(both_blc)) + geom_density() #graph key variable blame
+#seems to have huge bump at 1...
+
+optdata <- optdata %>% filter_at(vars(c(1:9)), all_vars(. != 9 & . != 1)) #exclude endpoints
+ggplot(optdata, aes(both_blc)) + geom_density()
+#now looks more normal/less skewed
